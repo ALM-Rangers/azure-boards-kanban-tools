f6c2e3b3717b715d2e21995dfb5b1de26aa05515
Madis Kõosaar <madis.koosaar@solidify.dev>
2022-03-25 14:43:37 +0000

    update deps, improve compilation

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,75 +1,18 @@
-const path = require("path");
-const fs = require("fs");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { merge } = require("webpack-merge");
+const CommonConfig = require("./webpack.base.config");
 
-module.exports = {
-  target: "web",
+module.exports = merge(CommonConfig, {
   mode: 'development',
-  entry: {
-    kanban: "./src/Kanban.ts",
-    kanbanDialog: "./src/KanbanPanel.tsx"
-  },
   output: {
-    filename: "src/[name].js",
-    path:  path.resolve(__dirname, 'dist'),
+    publicPath: "https://localhost:9090/dist"
   },
-  devtool: "inline-source-map",
   devServer: {
     https: true,
     port: 9090,
     open: true
   },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    alias: {
-      "azure-devops-extension-sdk": path.resolve("node_modules/azure-devops-extension-sdk"),
-      "VSSUI": path.resolve(__dirname, "node_modules/azure-devops-ui")
-    },
-    modules: [path.resolve("."), "node_modules"]
-
-  },
   stats: {
     warnings: false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader"
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "azure-devops-ui/buildScripts/css-variables-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.woff$/,
-        use: [
-          {
-            loader: "base64-inline-loader"
-          }
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif|html)$/,
-        use: "file-loader"
-      }
-    ]
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: "**/*.html", to: "./src", context: "src" },
-        { from: "**/*.png", to: "./img", context: "img" },
-      ]
-    })
-  ]
-};
+  }
+
+});
